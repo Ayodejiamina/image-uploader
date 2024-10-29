8cd3f346fbb23578e33183074343cc687281b122
Ayodeji amina <ayodjiamina.gmail.com>
2024-10-29 02:23:17 -0700

    done with uploader

--- src/Components/UploadMe.tsx
+++ src/Components/UploadMe.tsx
@@ -4,12 +4,12 @@ import "./UploadMe.css";
 function UploadMe() {
   const [uploadProfile, setUploadprofile] = useState<string | null>(null);
   const [uploadCover, setUploadcover] = useState<string | null>(null);
+  const [chosenOne, setChosenOne] = useState<string | null>(null);
   const [overlay, setOverlay] = useState<boolean>(false);
 
   function handleCoverPhoto(e: ChangeEvent<HTMLInputElement>) {
     e.preventDefault();
     const coverFile = e.target.files?.[0];
-    console.log(coverFile);
     if (coverFile) {
       const reader = new FileReader();
       reader.onloadend = () => {
@@ -27,14 +27,30 @@ function UploadMe() {
     marginTop: "10px",
     padding: "50px",
     borderRadius: "2px",
+    objectFit: "cover" as const,
   };
 
-  //Uploading Profile image
-  function handleProfile(e: ChangeEvent<HTMLInputElement>) {
+  const changeBacgroundProfile = {
+    backgroundColor: uploadProfile ? "transparent" : "blueviolet",
+    backgroundImage: uploadProfile ? `url(${uploadProfile})` : "none",
+    width: "70px",
+    display: "flex",
+    justifySelf: "center",
+    height: "70px",
+    borderRadius: "50%",
+    position: "relative" as const,
+    backgroundSize: "100% 100%",
+  };
+
+  // Uploading Profile image
+  function targetInput() {
+    const profileInput: HTMLElement | null = document.getElementById("profile");
+    profileInput?.click();
+  }
+
+  function profileInput(e: ChangeEvent<HTMLInputElement>) {
     e.preventDefault();
-
     const profile = e.target.files?.[0];
-
     if (profile) {
       const readProfile = new FileReader();
       readProfile.onloadend = () => {
@@ -44,19 +60,36 @@ function UploadMe() {
     }
   }
 
-  function handleOverlay() {
-    setOverlay((changeOverlay) => !changeOverlay);
+  function handleCoverOverlay() {
+    if (uploadCover) {
+      setChosenOne(uploadCover);
+      setOverlay(true);
+    }
+  }
+
+  function handleProfileOverlay(e: React.MouseEvent) {
+    e.stopPropagation();
+    if (uploadProfile) {
+      setChosenOne(uploadProfile);
+      setOverlay(true);
+    }
   }
 
   function closeOverlay() {
     setOverlay(false);
+    setChosenOne(null);
   }
+
   return (
     <>
       <div className={`container`}>
         <div className="subCont">
           <h2 className="logo">Image Uploader </h2>
-          <div className="cont1" style={changeBacgroundCover}>
+          <div
+            className="cont1"
+            style={changeBacgroundCover}
+            onClick={handleCoverOverlay}
+          >
             <div className="svg1">
               <label htmlFor="cover">
                 <svg
@@ -78,34 +111,31 @@ function UploadMe() {
                 onChange={handleCoverPhoto}
               />
             </div>
-            <div className="svg2">
-              <label
-                htmlFor="profile"
-                onClick={uploadProfile ? handleOverlay : undefined}
+
+            <div
+              className="svg2"
+              style={changeBacgroundProfile}
+              onClick={handleProfileOverlay}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 32 32"
+                onClick={targetInput}
               >
-                {uploadProfile ? (
-                  <img src={uploadProfile} alt="My profile Photo" />
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 32 32"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="m11.5 6l-.313.406L10 8H3v18h26V8h-7l-1.188-1.594L20.5 6zm1 2h7l1.188 1.594L21 10h6v14H5V10h6l.313-.406zM8 11a1 1 0 1 0 0 2a1 1 0 0 0 0-2m8 0c-3.302 0-6 2.698-6 6s2.698 6 6 6s6-2.698 6-6s-2.698-6-6-6m0 2c2.22 0 4 1.78 4 4s-1.78 4-4 4s-4-1.78-4-4s1.78-4 4-4"
-                    />
-                  </svg>
-                )}
-              </label>
+                <path
+                  fill="currentColor"
+                  d="m11.5 6l-.313.406L10 8H3v18h26V8h-7l-1.188-1.594L20.5 6zm1 2h7l1.188 1.594L21 10h6v14H5V10h6l.313-.406zM8 11a1 1 0 1 0 0 2a1 1 0 0 0 0-2m8 0c-3.302 0-6 2.698-6 6s2.698 6 6 6s6-2.698 6-6s-2.698-6-6-6m0 2c2.22 0 4 1.78 4 4s-1.78 4-4 4s-4-1.78-4-4s1.78-4 4-4"
+                />
+              </svg>
+
               <input
                 type="file"
                 name=""
                 id="profile"
                 accept="image/*"
-                onChange={handleProfile}
-                disabled={!!uploadProfile}
+                onChange={profileInput}
               />
             </div>
           </div>
@@ -119,7 +149,6 @@ function UploadMe() {
               <label htmlFor="">Link</label>
               <div className="url">
                 <input type="url" />
-
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="1em"
@@ -144,11 +173,11 @@ function UploadMe() {
 
       <div
         className={`overlay ${overlay ? "" : "hidden"}`}
-        onClick={overlay ? closeOverlay : handleOverlay}
+        onClick={closeOverlay}
       >
         <div className="overlay-text animate__animated animate__backInDown">
           <img
-            src={uploadProfile as string}
+            src={chosenOne as string}
             alt="My profile Photo"
             onClick={(e) => e.stopPropagation()}
           />
